04f713d0911fc331a4717904c248cde58cd65809
yanvasilij <yanvasilij@mail.ru>
2017-01-09 18:07:02 +0500

    Fixed error Runtime error on serial read occured sometimes.

--- yaplcconnectors/MK200/MK200Serial.py
+++ yaplcconnectors/MK200/MK200Serial.py
@@ -12,16 +12,21 @@ class MK200Serial (YaPySerial):
     def Read(self, nbytes):
         try:
             buf = ctypes.create_string_buffer( int(nbytes) )
-            res = int(self._SerialRead( ctypes.byref( self.port ), ctypes.cast( buf, ctypes.c_void_p ), ctypes.c_size_t( nbytes ) ))
+            int(self._SerialRead( ctypes.byref( self.port ), ctypes.cast( buf, ctypes.c_void_p ), ctypes.c_size_t( nbytes ) ))
             tmp = str(buf.raw)
             tmp = tmp.replace("\0", "")
             if len(tmp) == 0:
                 self.Write(self.TxBuf)
                 time.sleep(0.05)
-                res = int(self._SerialRead( ctypes.byref( self.port ), ctypes.cast( buf, ctypes.c_void_p ), ctypes.c_size_t( nbytes ) ))
+                int(self._SerialRead( ctypes.byref( self.port ), ctypes.cast( buf, ctypes.c_void_p ), ctypes.c_size_t( nbytes ) ))
         except:
-            raise YaPySerialError("Runrtime error on serial read!")
+            raise YaPySerialError("Runtime error on serial read!")
         tmp = str(buf.raw)
         response = tmp.replace("\0", "")
         return response
+
+    def Write(self, buf):
+        YaPySerial.Write(self, buf)
+        self.TxBuf = buf
+
 
