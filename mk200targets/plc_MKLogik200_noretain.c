ea4731899b4ea7861b242c9b1531a3d509ba5e32
yanvasilij <yanvasilij@mail.ru>
2017-03-22 16:10:36 +0500

    Add logs to plc_MKLogik200_noretaio.c

--- mk200targets/plc_MKLogik200_noretain.c
+++ mk200targets/plc_MKLogik200_noretain.c
@@ -1,20 +1,27 @@
+#include <stdio.h>
+
 int CheckRetainBuffer(void)
 {
+	printf("CheckRetainBuffer\r\n");
 	return 1;
 }
 
 void ValidateRetainBuffer(void)
 {
+	printf("ValidateRetainBuffer\r\n");
 }
 
 void InValidateRetainBuffer(void)
 {
+	printf("InValidateRetainBuffer\r\n");
 }
 
 void Retain(unsigned int offset, unsigned int count, void *p)
 {
+	printf("Retain: %u, %u, %x\r\n", offset, count, (unsigned int)*p);
 }
 
 void Remind(unsigned int offset, unsigned int count, void *p)
 {
+	printf("Remind: %u, %u, %x\r\n", offset, count, (unsigned int)*p);
 }
