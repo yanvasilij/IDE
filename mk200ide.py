273323a0fc0f8ab5a2381f29cebb32ab40d0b1bb
yanvasilij <yanvasilij@mail.ru>
2017-05-12 12:05:50 +0500

    Added getters and setters to modbus slave lib, removed old verstion of modbus slave

--- mk200ide.py
+++ mk200ide.py
@@ -25,12 +25,7 @@ class MK200Beremiz (BeremizIDELauncher):
     def ImportModules(self):
         import features
         features.catalog = [
-            ('canfestival', _('CANopen support'), _('Map located variables over CANopen'), 'canfestival.canfestival.RootClass'),
             ('c_ext', _('C extension'), _('Add C code accessing located variables synchronously'), 'c_ext.CFile'),
-            ('py_ext', _('Python file'), _('Add Python code executed asynchronously'), 'py_ext.PythonFile'),
-            ('wxglade_hmi', _('WxGlade GUI'), _('Add a simple WxGlade based GUI.'), 'wxglade_hmi.WxGladeHMI'),
-            ('svgui', _('SVGUI'), _('Experimental web based HMI'), 'svgui.SVGUI'),
-            ('MK200ModbusSlave', _('MK200 Modbus slave'), _('Plugin for modbus slave'), 'mk200modules.ModbusSlave.ModbusSlaveFile'),
             ('MKLogic201', _('MKLogic 201'), _('Plugin for MK201'), 'mk200modules.MK201.MK201ModuleFile'),
             ('MK200ModubsRequest', _('MK200 Modbus master request'), _('Plugin for MK201'), 'mk200modules.ModbusMaster.MK200ModbusRequestFile')]
 
