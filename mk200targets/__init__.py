4c28710f39c8cb8ebeabe09d205362aa6f5e98c4
yanvasilij <yanvasilij@mail.ru>
2017-06-02 15:38:05 +0500

    Added creatin time update in files in PROJECT_FOLDER/src filder for forced compilation all user C-files

--- mk200targets/__init__.py
+++ mk200targets/__init__.py
@@ -40,16 +40,41 @@ class MKLogik200_target(toolchain_gcc):
         return additionalobjects + toolchain_gcc.getBuilderLDFLAGS(self) + stm32_ldflags + ldscript# + ["-shared"]
 
     def getBuilderCFLAGS(self):
-        includePathes = [r'-I ' + "\"" + os.path.dirname(os.path.realpath(__file__)) + r'\beremizStm32Port\source\inc' + "\""]
-        includePathes.append(r'-I' + "\"" + os.path.dirname(os.path.realpath(__file__)) + r'\..\..\beremizStm32Port\modbusMasterLib\inc' + "\"" )
-        includePathes.append(r'-I' + "\"" + os.path.dirname(os.path.realpath(__file__)) + r'\..\..\FreeModbus\xCpp\include' + "\"")
-        includePathes.append(r'-I' + "\"" + os.path.dirname(os.path.realpath(__file__)) + r'\..\..\beremizStm32Port\source\inc' + "\"")
+        includePathes = [r'-I ' + "\"" + os.path.dirname(os.path.realpath(__file__)) + r'\..\..\beremizStm32Port\inc' + "\""]
+        """ Adding folder ./inc and ./src to include pathes """
         additionincludepath = os.path.split(self.exe_path)[0]
         additionincludepath = os.path.split(additionincludepath)[0]
-        additionincludepath = os.path.join(additionincludepath, "inc")
-        includePathes.append(r'-I' + "\"" + additionincludepath + "\"")
-        stm32_cflags = self.getSTM32Config("cflags") + includePathes
+        incFolder = os.path.join(additionincludepath, "inc")
+        srcFolder = os.path.join(additionincludepath, "src")
+        includePathes.append(r'-I' + "\"" + incFolder + "\"")
+        includePathes.append(r'-I' + "\"" + srcFolder + "\"")
+        stm32_cflags = self.getSTM32Config("cflags") + ["-std=c99"] + includePathes
         return toolchain_gcc.getBuilderCFLAGS(self) + stm32_cflags # + ["-fPIC"]
+
+    @staticmethod
+    def update_creation_time(path):
+        f = open(path, "a")
+        f.write(" ")
+        f.seek(-1, os.SEEK_END)
+        f.truncate()
+        f.close()
+
+    def build(self):
+        srcDir = os.path.split(self.exe_path)[0]
+        srcDir = os.path.split(srcDir)[0]
+        srcDir = os.path.join(srcDir, "src")
+        if os.path.isdir(srcDir):
+            listOfSrcFiles = [f for f in os.listdir(srcDir) if os.path.isfile(os.path.join(srcDir, f))]
+            listOfSrcFiles = [f for f in listOfSrcFiles if f.endswith(".c") or f.endswith(".cpp")]
+            cfileAndCFlag = []
+            for srcFile in listOfSrcFiles:
+                fileName = os.path.join(srcDir, srcFile)
+                """For forced recompilation there is need to update creation time"""
+                self.update_creation_time(fileName)
+                cfileAndCFlag.append((fileName, ''))
+            cfileAndCFlag = ('', cfileAndCFlag, True)
+            self.CTRInstance.LocationCFilesAndCFLAGS.append(cfileAndCFlag)
+        return toolchain_gcc.build(self)
 
 _base_path = os.path.split(__file__)[0]
 mk200targets = {'MKLogik200': {'code': {'plc_MKLogik200_main.c': os.path.join(_base_path, 'plc_MKLogik200_main.c')},
